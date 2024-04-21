bdab87ed963e1c14d9c155ae98c9ef2663314a73
samsnwn <samuel.c.glantz@gmail.com>
2024-04-22 00:31:30 +0200

    strmapi

---
+++ srcs/ft_strmapi.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int		ft_strlen(const char *s);
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	int		i;
+	char	*buf;
+
+	i = 0;
+	buf = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (buf == NULL)
+		return (NULL);
+	while (s[i])
+	{
+		buf[i] = f(i, s[i]);
+		i++;
+	}
+	buf[i] = '\n';
+	return (buf);
+}
+
+int	ft_strlen(const char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
