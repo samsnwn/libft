4d48015b94be28b64bdce565e3caea6b67352ce4
samsnwn <samuel.c.glantz@gmail.com>
2024-04-29 19:20:18 +0200

    bug

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -15,20 +15,20 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
-	t_list	*new_obj;
+	t_list	*temp;
 
 	if (!lst || !f || !del)
 		return (NULL);
 	new = NULL;
 	while (lst)
 	{
-		new_obj = ft_lstnew(f(lst->content));
-		if (!new_obj)
+		temp = ft_lstnew(f(lst->content));
+		if (!temp)
 		{
 			ft_lstclear(&new, del);
 			return (NULL);
 		}
-		ft_lstadd_back(&new, new_obj);
+		ft_lstadd_back(&new, temp);
 		lst = lst->next;
 	}
 	return (new);
