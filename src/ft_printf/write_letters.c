8a260067e35a86f50b6268fa76583b534ac3590d
samsnwn <samuel.c.glantz@gmail.com>
2024-09-25 22:09:11 +0200

    last changes

--- src/ft_printf/write_letters.c
+++ src/ft_printf/write_letters.c
@@ -12,6 +12,17 @@
 
 #include "ft_printf.h"
 
+size_t	ft_strlen(const char *s)
+
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
 int	write_char(int c)
 {
 	return (write(1, &c, 1));
